978fc2e4e3e0b24bcbd01f860d1b6d265c9cf1c7
Gianni Carlo <gcarlo89@hotmail.com>
2015-02-02 19:01:29 -0500

    Initial Commit

---
+++ CriptextKit/CriptextKit.h
@@ -0,0 +1,19 @@
+//
+//  CriptextKit.h
+//  CriptextKit
+//
+//  Created by Gianni Carlo on 2/2/15.
+//  Copyright (c) 2015 Criptext. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CriptextKit.
+FOUNDATION_EXPORT double CriptextKitVersionNumber;
+
+//! Project version string for CriptextKit.
+FOUNDATION_EXPORT const unsigned char CriptextKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CriptextKit/PublicHeader.h>
+
+
